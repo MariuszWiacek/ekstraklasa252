c974e91d0a29a9d1ebb7150930a3c2839972e18e
MariuszLillup <Mariusz@lillup.com>
2025-07-14 19:17:10 +0100

    gotowe

--- src/App.js
+++ src/App.js
@@ -14,13 +14,13 @@ import History from './pages/history';
 import Stats from './pages/stats';
 import Rules from './pages/rules';
 import Loading from './components/loading';
-import SignupPage from './pages/poll';
+
 import pitch from './images/pitc.jpeg';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
-  const [showSignup, setShowSignup] = useState(true); // 👈 State to show/hide popup
+  
 
   useEffect(() => {
     const handleImageLoad = () => {
@@ -77,7 +77,7 @@ function App() {
               </Routes>
             </div>
             <Footer />
-            {showSignup && <SignupPage onClose={() => setShowSignup(false)} />} {/* 👈 Popup */}
+            
           </div>
         )}
       </AnimatePresence>
